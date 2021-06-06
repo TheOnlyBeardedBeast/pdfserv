dc3095d3602ed9ded5cf5f4574265139245505fa
TheOnlyBeardedBeast <55414686+TheOnlyBeardedBeast@users.noreply.github.com>
2021-06-06 23:13:12 +0200

    Added separate i18n instance per request

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { renderToStream, Document } from "@react-pdf/renderer";
 import { Description } from "./Pages/Description";
+import { I18nextProvider } from "react-i18next";
 import { Intro } from "./Pages/Intro";
 import * as React from "react";
 import i18n from "i18next";
@@ -22,7 +23,8 @@ app.get("/:lang", async (req, res) => {
   const downloadParam = req.query["download"];
   const forceDownload = downloadParam === "" || !!downloadParam;
 
-  i18n.changeLanguage(req.params.lang);
+  const i18instance = i18n.cloneInstance();
+  i18instance.changeLanguage(req.params.lang ?? "en");
 
   if (forceDownload) {
     res.setHeader("Content-Type", "octet/stream");
@@ -31,6 +33,10 @@ app.get("/:lang", async (req, res) => {
     res.setHeader("Content-Type", "application/pdf");
   }
 
-  const stream = await renderToStream(<MyDocument />);
+  const stream = await renderToStream(
+    <I18nextProvider i18n={i18instance}>
+      <MyDocument />
+    </I18nextProvider>
+  );
   stream.pipe(res);
 });
